69cb56de40cc2ab1a7d3e8e96a302744f7844c1d
coco875 <59367621+coco875@users.noreply.github.com>
2022-01-26 23:27:25 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
-C:\Users\Corentin\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts\pyinstaller.exe --onefile --noconsole main.py
+pyinstaller --onefile --noconsole main.py
 cp -r language dist
 cp -r vocabulary dist
 cp config.json dist
