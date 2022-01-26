f597cbe3e81e089c000daa87c3efa58e2eaaa25c
coco875 <59367621+coco875@users.noreply.github.com>
2022-01-26 22:35:02 +0100

    b

--- compile.bat
+++ compile.bat
@@ -1,4 +1,6 @@
-pyinstaller --onefile --noconsole main.py
+@echo off
+C:\Users\Corentin\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts\pyinstaller.exe --onefile --noconsole main.py
 cp -r language dist
 cp -r vocabulary dist
 cp config.json dist
+zip -r app.zip dist
