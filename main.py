1111c64f4a177d41629d3076339d6364a7a91114
coco875 <59367621+coco875@users.noreply.github.com>
2022-01-31 15:05:15 +0100

    b

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from types import NoneType
 import PySimpleGUI as sg
 from os import listdir
 from os.path import isfile, join
@@ -41,9 +42,9 @@ layout_menu:list = [
 edit = False
 # ------ Make the Table Data ------
 # sg.Print('Creating table...')
-data = category[0]["list_word"]
+data = [["",""]]
 # headings = [str(data[0][x])+'     ..' for x in range(len(data[0]))]
-headings = category[0]["language"]
+headings = category[0]["category"]
 # sg.Print('Done creating table.  Creating GUI...')
 
 category_name : list = []
@@ -51,13 +52,12 @@ category_name : list = []
 convert_title_category:dict = {}
 
 for i in category:
-    if equivalence_lang[config["lang"]] in i["language"]:
-        convert_title_category[i["title"][config["lang"]]] = category.index(i)
-        category_name.append(i["title"][config["lang"]])
-print(category_name)
+    if lang["short_name"] in i["language"]:
+        convert_title_category[i["title"][lang["short_name"]]] = category.index(i)
+        category_name.append(i["title"][lang["short_name"]])
 
 layout_category: list = [
-    [sg.Listbox(category_name, key="cat", select_mode="LISTBOX_SELECT_MODE_SINGLE")],
+    [sg.Listbox(category_name, key="-category-", select_mode="LISTBOX_SELECT_MODE_SINGLE")],
     [sg.Button(lang["menu"]["continue"]), sg.Button(lang["menu"]["back"]), sg.Button(lang["menu"]["new_list"])]
 ]
 
@@ -89,17 +89,21 @@ menu_def:list = [
 SelectLang = lambda lkey: sg.Input(do_not_clear=True, size=(22,1), key=lkey, pad=(0,2))
 
 layout_make_list = [
-    [sg.Text(lang["menu"]["name_file"]), sg.Input(key="name_file")],
+    [sg.Text(lang["menu"]["name_file"]), sg.Input(key="name_file",size=(36,1)), sg.Text(".json")],
     [
         sg.Text(lang["menu"]["lang"]+" :"),
         SelectLang("lang1"),
         sg.Text(" "),
         SelectLang("lang2")],
+    [
+        sg.Text(lang["menu"]["category"]+" :"),
+        SelectLang("cat1"),
+        sg.Text(" "),
+        SelectLang("cat2")],
     [sg.Table(values=data, headings=headings, max_col_width=25,
                         auto_size_columns=True,
                         # display_row_numbers=True,
                         justification='right',
-                        num_rows=20,
                         alternating_row_color=sg.theme_button_color()[1],
                         key='-TABLE-',
                         # selected_row_colors='red on yellow',
@@ -108,44 +112,52 @@ layout_make_list = [
                         expand_x=True,
                         expand_y=True,
                         enable_click_events=True,  # Comment out to not enable header and other clicks
-                        )]
+                        )],
+    [sg.Button(lang["menu"]["save"]), sg.Button(lang["menu"]["add_line"])]
 ]
 
 #define layout
 layout:list = [
     [sg.Menu(menu_def, background_color='lightsteelblue', text_color='navy', disabled_text_color='yellow', font='Verdana', pad=(10, 10))],
     [
-        sg.Column(layout_menu, key='menu',visible=True), 
-        sg.Column(layout_category, key='category',visible=False),
-        sg.Column(layout_option_mode, key='mode',visible=False),
-        sg.Column(layout_quest, key='quest',visible=False),
-        sg.Column(layout_responce, key='responce', visible=False),
-        sg.Column(layout_total, key='total', visible=False),
-        sg.Column(layout_make_list, key='make_list', visible=False)
+        sg.Column(layout_menu, key='menu',visible=True, element_justification="center", vertical_alignment='center'), 
+        sg.Column(layout_category, key='category',visible=False, element_justification="center", vertical_alignment='center'),
+        sg.Column(layout_option_mode, key='mode',visible=False, element_justification="center", vertical_alignment='center'),
+        sg.Column(layout_quest, key='quest',visible=False, element_justification="center", vertical_alignment='center'),
+        sg.Column(layout_responce, key='responce', visible=False, element_justification="center", vertical_alignment='center'),
+        sg.Column(layout_total, key='total', visible=False, element_justification="center", vertical_alignment='center'),
+        sg.Column(layout_make_list, key='make_list', visible=False, element_justification="center", vertical_alignment='center')
     ]
 ]
 
 # Create the window
-window:sg.Window = sg.Window(lang["menu"]["name"], layout, finalize=True, size=(600,300), grab_anywhere=True, element_justification='c', font=('Courier', 12))
+window: sg.Window = sg.Window(lang["menu"]["name"], layout, finalize=True, element_justification="c", size=(900, 450), font=('Courier', 12))
 window.bind("<Return>", "+Enter+")
 window.bind("<Escape>", "-Escape-")
+window["cat1"].bind("<KeyPress>","update_cat")
+window["cat2"].bind("<KeyPress>", "update_cat")
 
+def func():
+    pass
 
 def edit_cell(window, key, row, col, justify='right'):
 
-    global textvariable, edit
+    global textvariable, edit, func
 
-    def callback(event, row, col, text, key):
+    def callback(event, row, col, text, key, original_key):
         global edit
-        widget = event.widget
+        if "widget" in event.__dict__:
+            widget = event.widget
+        else:
+            widget = event
+
         if key == 'Return':
             text = widget.get()
-            print(text)
         widget.destroy()
         widget.master.destroy()
-        values = list(table.item(row, 'values'))
-        values[col] = text
-        table.item(row, values=values)
+        values = window[original_key].Get()
+        values[row-1][col] = text
+        window[original_key].update(values=values)
         edit = False
 
     if edit or row <= 0:
@@ -154,8 +166,7 @@ def edit_cell(window, key, row, col, justify='right'):
     edit = True
     root = window.TKroot
     table = window[key].Widget
-
-    text = table.item(row, "values")[col]
+    text = window[key].Get()[row-1][col]
     x, y, width, height = table.bbox(row, col)
 
     frame = sg.tk.Frame(root)
@@ -169,17 +180,23 @@ def edit_cell(window, key, row, col, justify='right'):
             y_tmp = max(y_tmp, y_temp)
         add_y += y_tmp
 
-    add_y += 17
-    frame.place(x=x, y=y+add_y, anchor="nw", width=width, height=height)
+    add_y += 5
+    frame.place(x=x+10, y=y+add_y, anchor="nw", width=width, height=height)
     textvariable = sg.tk.StringVar()
     textvariable.set(text)
-    entry = sg.tk.Entry(frame, textvariable=textvariable, justify=justify, font=('Courier', 12), borderwidth=0)
+    entry = sg.tk.Entry(frame, 
+                        textvariable=textvariable, 
+                        justify=justify, 
+                        font=('Courier', 12), 
+                        borderwidth=0,
+                        highlightthickness=0)
     entry.pack()
     entry.select_range(0, sg.tk.END)
     entry.icursor(sg.tk.END)
     entry.focus_force()
-    entry.bind("<Return>", lambda e, r=row, c=col, t=text, k='Return':callback(e, r, c, t, k))
-    entry.bind("<Escape>", lambda e, r=row, c=col, t=text, k='Escape':callback(e, r, c, t, k))
+    entry.bind("<Return>", lambda e, r=row, c=col, t=text, k='Return',o=key:callback(e, r, c, t, k, o))
+    func = lambda e=entry, r=row, c=col, t=text, k='Return',o=key:callback(e, r, c, t, k, o)
+    entry.bind("<Escape>", lambda e, r=row, c=col, t=text, k='Escape',o=key:callback(e, r, c, t, k, o))
 
 def restart_program():
     """Restarts the current program, with file objects and descriptors
@@ -212,14 +229,26 @@ while True:
     # End program if user closes window or
     # presses the OK button
 
-    
+    if edit and "+CICKED+" in event:
+        func()
 
     if event == lang["menu"]["close"] or event == sg.WIN_CLOSED:
         break
 
-    elif isinstance(event, tuple):
-        row, col = event[2]
-        edit_cell(window, '-TABLE-', row+1, col, justify='right')
+    elif window["make_list"].visible:
+        if event == lang["menu"]["add_line"]:
+            window["-TABLE-"].update(values=window["-TABLE-"].Get()+[["", ""]])
+
+        elif isinstance(event, tuple):
+            row, col = event[2]
+            if type(row) != NoneType:
+                edit_cell(window, '-TABLE-', row+1, col, justify='right')
+        
+        elif event == "+Enter+" or event == "cat1update_cat" or event == "cat2update_cat":
+            table = window['-TABLE-'].Widget
+            headings = [values["cat1"], values["cat2"]]
+            for cid, text in zip(window["-TABLE-"].ColumnHeadings, headings):
+                table.heading(cid, text=text)
 
     elif event in all_lang:
         config["lang"] = event[:2]
@@ -235,12 +264,12 @@ while True:
         window["menu"].update(visible=False)
         window["category"].update(visible=True)
     
-    elif (event == "+Enter+" or event.startswith(lang["menu"]["continue"])) and window["category"].visible and values["cat"] != []:
-        selected_categorie= category[convert_title_category[values["cat"][0]]]
+    elif (event == "+Enter+" or event.startswith(lang["menu"]["continue"])) and window["category"].visible and values["-category-"] != []:
+        selected_categorie= category[convert_title_category[values["-category-"][0]]]
         window["category"].update(visible=False)
         window["mode"].update(visible = True)
         option_mode=[]
-        for i in getCombinations(selected_categorie["language"]):
+        for i in getCombinations(selected_categorie["category"]):
             option_mode.append(i[0]+f" {lang['menu']['to']} "+i[1])
         option_mode.append(lang["menu"]["random"])
         window["option_mode"].update(values=option_mode)
@@ -248,7 +277,7 @@ while True:
     elif (event.startswith(lang["menu"]["continue"]) or event == "+Enter+") and values["option_mode"] != "":
         selected_mode = [f for f in values["option_mode"].split(" "+lang['menu']['to']+" ")]
         option_mode = []
-        for i in getCombinations(selected_categorie["language"]):
+        for i in getCombinations(selected_categorie["category"]):
             option_mode.append(i[0]+f" {lang['menu']['to']} "+i[1])
         option_mode.append(lang["menu"]["random"])
         window["option_mode"].update(values=option_mode)
@@ -259,10 +288,10 @@ while True:
         if selected_mode == [lang["menu"]["random"]]:
             for i in list_word:
                 random.shuffle(i)
-        elif selected_mode != selected_categorie["language"]:
+        elif selected_mode != selected_categorie["category"]:
             for i in list_word:
                 i.reverse()
-        window["question"].update(value=list_word[id_quest][0])
+        window["question"].update(value=lang["menu"]["translate"]+list_word[id_quest][0])
     
     elif (event == "+Enter+" or event.startswith(lang["menu"]["submit"])) and values["answer"] != "" and not window["responce"].visible:
         window["quest"].update(visible=False)
@@ -277,7 +306,7 @@ while True:
     elif (event.startswith(lang["menu"]["continue"]) or event == "+Enter+") and window["responce"].visible:
         id_quest+=1
         if id_quest!=len(list_word):
-            window["question"].update(value=list_word[id_quest][0])
+            window["question"].update(value=lang["menu"]["translate"]+list_word[id_quest][0])
             window["quest"].update(visible=True)
             window["responce"].update(visible=False)
         else:
